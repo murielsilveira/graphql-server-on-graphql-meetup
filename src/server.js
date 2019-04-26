0e48cbec77352c2889ac1ad1f67dc7c35f5538f9
Muriel Silveira <murielkong@gmail.com>
2019-04-26 13:02:14 +0200

    chore: setup a graphql server with example query

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return '🙈'
+    }
+  },
+}
+
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
 const app = express()
 
+apolloServer.applyMiddleware({ app })
 app.get('/', (resquest, response) => {
   response.send('was the pizza good?')
 })
